694b8f00d2c904d1bdc032ae632c0563682d5592
Stujo <stujo@users.noreply.github.com>
2016-05-23 10:25:30 -0700

    Backbone View

--- js/app.js
+++ js/app.js
@@ -1,24 +1,40 @@
-(function(Backbone){
+(function(Backbone) {
 
-   var Rectangle = Backbone.Model.extend({});
+    var Rectangle = Backbone.Model.extend({});
 
-  var rect = new Rectangle({
-  	width: 150,
-  	height: 100,
-  	top: 5,
-  	left: 50,
-  	color: "#F00"
-  	});
+    var AppView = Backbone.View.extend({
+        el: '#container',
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            this.$el.html('<div id="canvas"><div class="rectangle"></div></div>');
+        }
+    });
+
+    var appView = new AppView();
 
 
-  function logColor(rectangle, newColor, context){
-  	console.log("logColor(" + newColor + ");");
-    console.log(rectangle.get("color"));
-  }
+    var rect = new Rectangle({
+        width: 150,
+        height: 100,
+        top: 5,
+        left: 50,
+        color: "#F00"
+    });
 
-  rect.on("change:color", logColor);
 
-  rect.set("color",'#0F0');
+    function logColor(rectangle, newColor, context) {
+        console.log("logColor(" + newColor + ");");
+        console.log(rectangle.get("color"));
+    }
+
+    rect.on("change:color", logColor);
+
+    rect.set("color", '#0F0');
+
+
+
+
 
 })(Backbone);
-
