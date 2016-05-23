38d073e9d90425b043b42c6160849ed5e1685544
Stujo <stujo@users.noreply.github.com>
2016-05-23 15:49:41 -0700

    Underscore Templates and Rectangle Array

--- js/app.js
+++ js/app.js
@@ -1,36 +1,49 @@
 (function(Backbone) {
 
+    var app = {}
+
     var Rectangle = Backbone.Model.extend({});
 
-    var rect = new Rectangle({
-        width: 150,
-        height: 100,
-        top: 50,
-        left: 50,
-        color: "#F00"
-    });
+    var rectangles = [new Rectangle({
+            id: 1,
+            width: 150,
+            height: 100,
+            top: 5,
+            left: 5,
+            color: "#00F"
+        }),
+        new Rectangle({
+            id: 2,
+            width: 150,
+            height: 100,
+            top: 90,
+            left: 100,
+            color: "#0F0"
+        })
+    ];
 
-    var AppView = Backbone.View.extend({
+    app.AppView = Backbone.View.extend({
         el: '#container',
-        template: _.template('<div id="canvas"><div class="rectangle" style="top: <%= model.get("top") %>px"></div></div>'),
-      initialize: function() {
+        template: _.template($('#canvas-template').html()),
+        rectangleTemplate: _.template($('#rectangle-template').html()),
+        initialize: function() {
             this.render();
         },
         render: function() {
-            this.$el.html(this.template({model: rect}));
+            this.$el.html(this.template({
+                model: this.model,
+                templates: {
+                    rectangle: this.rectangleTemplate
+                }
+            }));
         }
     });
 
-    var appView = new AppView();
+    app.appView = new app.AppView({
+        model: {
+            rectangles: rectangles
+        }
+    });
 
-
-    function logColor(rectangle, newColor, context) {
-        console.log("logColor(" + newColor + ");");
-        console.log(rectangle.get("color"));
-    }
-
-    rect.on("change:color", logColor);
-
-    rect.set("color", '#0F0');
 
 })(Backbone);
